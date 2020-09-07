74abf2abb8552b4240061df6a75c61ccc2e6c722
Taylor B <tbrit10@gmail.com>
2020-09-06 21:56:41 -0700

    Add Password field to UserModel
    - Add Password field, which generates a salt, hashes the given password, and sets the UserModel hashedpassword and salt to the generated values

--- Models/UserModel.cs
+++ Models/UserModel.cs
@@ -2,6 +2,8 @@
 using System.Collections.Generic;
 using System.ComponentModel.DataAnnotations;
 using System.Linq;
+using System.Security.Cryptography;
+using System.Text;
 using System.Threading.Tasks;
 
 namespace App.Models
@@ -13,6 +15,18 @@ namespace App.Models
         public string Email { get; set; }
         public string Name { get; set; }
         public string HashedPassword { get; set; }
+        public string Password { set
+            {
+                //TODO: Generate this each time instead of using a hardcoded value
+                this.Salt = "test12";
+
+                SHA1 sha = SHA1.Create();
+                string passAndSalt = value + this.Salt;
+                byte[] passAndSaltBytes = Encoding.ASCII.GetBytes(passAndSalt);
+                byte[] newHash = sha.ComputeHash(passAndSaltBytes);
+                string newHashString = string.Concat(newHash.Select(b => b.ToString("X2")));
+                this.HashedPassword = newHashString;
+            } }
         public string Salt { get; set; }
     }
 }
